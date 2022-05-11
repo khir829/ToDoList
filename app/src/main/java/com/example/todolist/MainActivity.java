d57ea72825eed2deeff357870ac9ab07cd51b04f
Kai <khir829@aucklanduni.ac.nz>
2022-05-12 00:00:46 +1200

    Add adapter class utility to main activity

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -6,10 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.todolist.adapter.ToDoAdapter;
+import com.example.todolist.model.ToDoModel;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView tasksRecyclerView;
-
+    private ToDoAdapter tasksAdapter;
+    private List<ToDoModel> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,18 @@ public class MainActivity extends AppCompatActivity {
 
         tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
         tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a test task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
     }
 }
