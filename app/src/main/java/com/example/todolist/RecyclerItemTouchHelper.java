409cca140e5035a1cf0a898079dd6f0a12969a00
Kai <khir829@aucklanduni.ac.nz>
2022-05-23 19:32:38 +1200

    Add RecyclerItemTouchHelper class

---
+++ app/src/main/java/com/example/todolist/RecyclerItemTouchHelper.java
@@ -0,0 +1,49 @@
+package com.example.todolist;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todolist.adapter.ToDoAdapter;
+
+public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+    private ToDoAdapter adapter;
+
+    public RecyclerItemTouchHelper(ToDoAdapter adapter){
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        this.adapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target){
+        return false;
+    }
+
+    @Override
+    public void onSwiped(final RecyclerView.ViewHolder viewHolder, int direction){
+        final int position = viewHolder.getAdapterPosition();
+        if(direction == ItemTouchHelper.LEFT){
+            AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
+            builder.setTitle("Delete Task");
+            builder.setMessage("Are you sure you want to delete this Task?");
+            builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    adapter.deleteItem(position);
+                }
+            });
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    adapter.notifyItemChanged(viewHolder.getAdapterPosition());
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        } else {
+            adapter.editItem(position);
+        }
+    }
+}
