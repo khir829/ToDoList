59ac68e0bb9375594857cc8e84e5d25d77421043
Kai <khir829@aucklanduni.ac.nz>
2022-05-23 19:51:24 +1200

    Add swiping function for app

--- app/src/main/java/com/example/todolist/RecyclerItemTouchHelper.java
+++ app/src/main/java/com/example/todolist/RecyclerItemTouchHelper.java
@@ -2,7 +2,13 @@ package com.example.todolist;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
 
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -45,5 +51,46 @@ public class RecyclerItemTouchHelper extends ItemTouchHelper.SimpleCallback {
         } else {
             adapter.editItem(position);
         }
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isActive){
+        super.onChildDraw(c,recyclerView,viewHolder,dX,dY,actionState,isActive);
+
+        Drawable icon;
+        ColorDrawable background;
+
+        View itemView = viewHolder.itemView;
+        int backgroundCornerOffset = 20;
+
+        if(dX>0){
+            icon = ContextCompat.getDrawable(adapter.getContext(),R.drawable.ic_baseline_edit_24);
+            background = new ColorDrawable(ContextCompat.getColor(adapter.getContext(), R.color.colorPrimaryDark));
+        } else {
+            icon = ContextCompat.getDrawable(adapter.getContext(),R.drawable.ic_baseline_delete_24);
+            background = new ColorDrawable(Color.RED);
+        }
+
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if (dX > 0){// Swiping to the right
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + ((int)dX) + backgroundCornerOffset, itemView.getBottom());
+        } else if (dX < 0){// Swiping to the left
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int)dX) - backgroundCornerOffset, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        } else {
+            background.setBounds(0, 0, 0, 0);
+        }
+        background.draw(c);
+        icon.draw(c);
     }
 }
